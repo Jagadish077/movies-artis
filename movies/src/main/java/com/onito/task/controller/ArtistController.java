da3a964eabdf6193773c6dd43bf31c591a682d29
Jagadish <jagadishjaga772@gmail.com>
2023-05-10 14:12:20 +0530

    added controllers

---
+++ movies/src/main/java/com/onito/task/controller/ArtistController.java
@@ -0,0 +1,10 @@
+package com.onito.task.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class ArtistController {
+
+}
